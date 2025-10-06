d317acc98e26b3903dba7fb2628f58ad0566158a
Kerem FURUNCU <keremfuruncu007@gmail.com>
2025-10-06 22:14:32 +0300

    Updated

---
+++ 07_ForeachLoop/07_ForeachLoop.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_ForeachLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Foreach Loop
+            //Foreach(1;2;3;4)
+            //1:variable type
+            //2:variable name
+            //3:In
+            //4:Liste, Kolesksiyon veya Dizi adi
+
+            //string[] cities = { "istanbul", "Ankara", "Bursa", "Edirne", "Vancouver", "Seul", "Berlin" };
+            //foreach (string city in cities)
+            //{
+            //    Console.WriteLine(city);
+            //}
+
+            //int[] numbers = { 45, 78, 985, 635, 74, 11, 22, 33, 41, 205, 6578, 10394 };
+            //foreach(int number in numbers)
+            //{
+            //    if(number%2==0)
+            //    {
+            //        Console.WriteLine(number + " is a double number");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(number + " is a double number");
+            //    }
+            //}   
+
+            //int[] numbers = { 45, 78, 985, 635, 74, 11, 22, 33, 41, 205, 6578, 10394 };
+            //int total = 0;
+            //foreach (int i in numbers)
+            //{
+            //    total = total + i;
+            //}
+            //Console.WriteLine(total);
+
+            //List<int> numbers = new List<int>()
+            //{
+            //    1,2,4,5,8                
+            //};
+            //foreach (int number in numbers)
+            //{
+            //    Console.WriteLine(number);
+            //}
+
+            //string word = "Merhaba";
+            //foreach(char c in word)
+            //{
+            //    Console.WriteLine(c);
+            //}            
+            #endregion
+
+            #region EX/Exam System App     
+            //Console.Write("Please Enter Students Count: ");
+            //int studentsCount = int.Parse(Console.ReadLine());
+            //string[] studentsName = new string[studentsCount];
+            //for (int i = 0; i < studentsCount; i++)
+            //{
+            //    Console.Write("Please Enter " + (i + 1) + ". Student Name: ");
+            //    studentsName[i] = Console.ReadLine();
+            //}
+            //double[] vize = new double[studentsCount];
+            //for (int i = 0; i < studentsCount; i++)
+            //{
+            //    Console.Write("Please Enter " + studentsName[i] + "`s Vize Result: ");
+            //    vize[i] = double.Parse(Console.ReadLine());
+            //    if (vize[i]>100 | vize[i]<0)
+            //    {
+            //        Console.WriteLine("Wrong Value!");
+            //        i--;
+            //    }
+            //}            
+            //double[] final = new double[studentsCount];
+            //for (int i = 0; i < studentsCount; i++)
+            //{
+            //    Console.Write("Please Enter " + studentsName[i] + "`s Final Result: ");
+            //    final[i] = double.Parse(Console.ReadLine());
+            //    if (final[i] > 100 | final[i]<0)
+            //    {
+            //        Console.WriteLine("Wrong Value!");
+            //        i--;
+            //    }
+            //}
+            //double[] examsAverage = new double[studentsCount];
+            //for (int i = 0; i < studentsCount; i++)
+            //{
+            //    examsAverage[i] = (vize[i] * 4 + final[i] * 6) / 10;
+            //    Console.WriteLine(studentsName[i] + "`s Exams Average: " + examsAverage[i]);
+            //}
+            //Console.WriteLine("---------------------------------------------------");
+            //for (int i = 0; i < studentsCount; i++)
+            //{
+            //    Console.WriteLine("Student Name: " + studentsName[i]);
+            //    Console.WriteLine("Vize Result: " + vize[i]);
+            //    Console.WriteLine("Final Result: " + final[i]);
+            //    Console.WriteLine("Exams Average: " + (vize[i] * 4 + final[i] * 6) / 10);
+            //    Console.WriteLine("---------------------------------------------------");
+            //}
+            //double examsAveragesTotal = 0;
+            //foreach (double i in examsAverage)
+            //{
+            //    examsAveragesTotal = examsAveragesTotal + i;
+            //}
+            //Console.WriteLine("All Students Exam Average: " + examsAveragesTotal / studentsCount);
+            #endregion
+        }
+    }
+}
---
+++ 08_Methods/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Void Metotlar
+            // Bir yapinin metot olarak algilanmasi icin sonuna parantez acilip kapatilmasi gerekiyor ==> ...()
+            // C# ta bir metotdun aciklamasina bakildiginda (mouse u ustune getirdiginde) gozuken aciklamanin basinda (en solunda) mor renkli bir kup gozukur.
+            // 1) Geriye deger dondurmeyen metotlar (void metotlar)
+
+            //void CustomerList()
+            //{
+            //    Console.WriteLine("Ali YILDIZ");
+            //    Console.WriteLine("Zeynep YILDIZ");
+            //    Console.WriteLine("Hakan ORMAN");
+            //    Console.WriteLine("Merve Fidan");
+            //}
+            //CustomerList();
+
+            //void Sum()
+            //{
+            //    int x = 1;
+            //    int y = 2;
+            //    int z = x + y;
+            //    Console.WriteLine(z);
+            //}
+            //Sum();
+            #endregion
+
+            #region Geriye Deger Dondurmeyen String Parametreli Metotlar
+            //void CustomerCard(string name, string surName)
+            //{
+            //    Console.WriteLine("Customer: " + name + " " + surName);
+            //}
+            //CustomerCard("Ahmet","Yildirim");
+            //CustomerCard("Kerem", "FURUNCU");
+            #endregion
+
+            #region Geriye Deger Dondurmeyen Integer Parametreli Metotlar
+            //void Sum(int number1, int number2, int number3)
+            //{
+            //    int result = number1 + number2 + number3;
+            //    Console.WriteLine(result);
+            //}
+            //Sum(13, 32, 61);
+            #endregion
+
+            #region Geriye Deger Donduren Metotlar
+            //string StudentCard()
+            //{
+            //    string name = "Egemen";
+            //    string surname = "Bey";
+            //    return name+" "+surname;
+            //}
+            //Console.WriteLine(StudentCard());
+            #endregion
+
+            #region Geriye Deger Donduren String Parametreli Metotlar
+            //string CountryCard(string countryName, string capitalName)
+            //{
+            //    string cardInfo = "Country: " + countryName + " / Capital: " + capitalName;
+            //    return cardInfo;
+            //}
+            //string x, y;
+            //Console.Write("Please Enter Your Country Name: ");
+            //x = Console.ReadLine();
+            //Console.Write("Please Enter Your Country's Capital: ");
+            //y = Console.ReadLine();
+            //Console.WriteLine(CountryCard(x, y));
+            #endregion
+
+            #region Geriye Deger Donduren Integer Parametreli Metotlar
+            //int Sum(int number1, int number2)
+            //{
+            //    int result = number1 + number2;
+            //    return result;
+            //}
+            //Console.WriteLine(Sum(41, 61));
+            //Console.WriteLine(Sum(6, 34));
+            #endregion
+
+            #region Ornek Uygulama
+            string ExamResult(string student, double vize, double final)
+            {
+                double average = (vize * 4 + final * 6) / 10;
+                if(average>=50)
+                {
+                    return student + " Adli Ogrenci Sinavi Gecti / Ortalamasi="+average;
+                }
+                else
+                {
+                    return student + " Adli Ogrenci Sinavdan Kaldi / Ortalamasi="+average;
+                }
+            }
+            Console.WriteLine(ExamResult("Kerem", 23,76));
+            #endregion
+        }
+    }
+}
